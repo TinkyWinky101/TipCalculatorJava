fedbc7db6eb7b62b42e4ebbae635834674164be6
TinkyWinky101 <tylervo02@yahoo.com>
2022-04-25 16:41:37 -0700

    Work lol

---
+++ src/TipGUI.java
@@ -0,0 +1,18 @@
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class TipGUI {
+    private JPanel panel1;
+    private JButton button1;
+    private JTextField textField1;
+
+    public TipGUI() {
+        button1.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+    }
+}
---
+++ src/calculate.java
@@ -0,0 +1,28 @@
+public class calculate {
+    /**
+     *
+     * @param serviceArray
+     * @param fullTip
+     *
+     *
+     */
+    public calculate(double[] serviceArray, double fullTip){
+        for(int i = 0; i < 7; i++){
+            if(serviceArray[i] != 0){
+                // Get percentage
+                double percentage = serviceArray[i] / total;
+                // Calculate tip by multiplying percentage with total of the tip
+                int calculatedTip =
+            }
+        }
+    }
+
+    /**
+     *
+     * @param serviceArray
+     * @return
+     */
+    private static int getTotal(double[] serviceArray){
+        for int i
+    }
+}
